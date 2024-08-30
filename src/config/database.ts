ef3f2349feeece63b017762186b1d1d2f71ada27
Silas L. Pascoal <slpascoal01@gmail.com>
2024-08-30 11:35:20 -0300

    Atualizar possível erro em db
    Caso o ".env" não possua "MONGO_URI", criada condicional com um endereço pré-definido.

--- src/config/database.ts
+++ src/config/database.ts
@@ -5,10 +5,11 @@ import mongoose from 'mongoose';
 
 // instanciar e executar o bd
 export const connectDB = async () => {
+  
   // tenta se conectar ao bd relacionado em process.env.MONGO_URI
   try {
 
-    await mongoose.connect(process.env.MONGO_URI as string, {});
+    await mongoose.connect(process.env.MONGO_URI || "mongodb://mongo:27017/teste" as string, {});
     console.log('MongoDB connected');
 
   } catch (error) {
